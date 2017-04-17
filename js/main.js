c6e252e76f6a9a8507366785744b338e81915b4c
John <johnfarrelly@gmail.com>
2017-04-16 20:18:20 -0400

    My real first commit because I incorrectly used versioning control previously

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Up and running!");
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push('cardOne');
+console.log("User flipped queen!")
