7f3aa4ca275eff09313ce76f40fe6f6f16e38fd5
John <johnfarrelly@gmail.com>
2017-04-16 20:30:16 -0400

    Created game logic, unit 8 assignment

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
+
 var cardOne = cards[0];
 cardsInPlay.push('cardOne');
-console.log("User flipped queen!")
+console.log("User flipped queen!");
+
+var cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+console.log("User flipped king!");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[1] === cardsInPlay[2]) {
+		alert("You found a match!");
+	} else {
+		alert("So sorry, no match :(");
+	}
+} 
