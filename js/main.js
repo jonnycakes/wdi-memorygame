08421ad39ce94e9324b9d12a616e26bdd815fdca
John <johnfarrelly@gmail.com>
2017-04-17 21:29:59 -0400

    Created logic, unit 9 assignment

--- js/main.js
+++ js/main.js
@@ -4,18 +4,28 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen!");
+var checkForMatch = function () {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("So sorry, no match :(");
+		}
+	} 
 
-var cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king!");
+}
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[1] === cardsInPlay[2]) {
-		alert("You found a match!");
-	} else {
-		alert("So sorry, no match :(");
-	}
-} 
+var flipCard = function (cardId) {
+	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId]);
+	
+	checkForMatch();
+
+}
+
+
+
+
+flipCard(0)
+flipCard(2)
+
