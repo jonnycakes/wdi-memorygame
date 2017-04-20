02d616abd5bdf6a6e359ae0157d75adc02192da7
John <johnfarrelly@gmail.com>
2017-04-20 10:19:22 -0400

    Created game logic, unit 10 assignment

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 console.log("Up and running!");
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+		{
+			rank: "queen",
+			suite: "hearts",
+			cardImage: "images/queen-of-hearts.png"
+		},
+		{
+			rank: "queen",
+			suite: "diamonds",
+			cardImage: "images/queen-of-diamonds.png"
+		},
+		{
+			rank: "king",
+			suite: "hearts",
+			cardImage: "images/king-of-hearts.png"
+		},
+		{
+			rank: "king",
+			suite: "diamonds",
+			cardImage: "images/king-of-diamonds.png"
+		}
+	];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -16,8 +38,10 @@ var checkForMatch = function () {
 }
 
 var flipCard = function (cardId) {
-	cardsInPlay.push(cards[cardId]);
-	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suite);
+	console.log(cards[cardId].cardImage);
 	
 	checkForMatch();
 
