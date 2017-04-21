5eeb43b2a505dc00a4286ff351fdf730c36bd702
John <johnfarrelly@gmail.com>
2017-04-20 20:50:04 -0400

    Created game, still kinks to work out. Unit 11 end

--- js/main.js
+++ js/main.js
@@ -5,51 +5,66 @@ var cards = [
 		{
 			rank: "queen",
 			suite: "hearts",
-			cardImage: "images/queen-of-hearts.png"
+			cardImage: "img/queen-of-hearts.png"
 		},
 		{
 			rank: "queen",
 			suite: "diamonds",
-			cardImage: "images/queen-of-diamonds.png"
+			cardImage: "img/queen-of-diamonds.png"
 		},
 		{
 			rank: "king",
 			suite: "hearts",
-			cardImage: "images/king-of-hearts.png"
+			cardImage: "img/king-of-hearts.png"
 		},
 		{
 			rank: "king",
 			suite: "diamonds",
-			cardImage: "images/king-of-diamonds.png"
+			cardImage: "img/king-of-diamonds.png"
 		}
 	];
 
 var cardsInPlay = [];
 
+var clearCards = function () {
+	cardsInPlay = [];
+	createBoard ();
+}
+
 var checkForMatch = function () {
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
+			alert("You found a match! Click OK to start a new game!");
+			clearCards ();
+
 		} else {
-			alert("So sorry, no match :(");
+			alert("So sorry, no match. sad face. Don't worry, you can try again by clicking OK. There's only so many matches, ya know?");
+			clearCards ();
 		}
 	} 
 
 }
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].suite);
 	console.log(cards[cardId].cardImage);
+	this.setAttribute('src', cards[cardId].cardImage);
 	
 	checkForMatch();
 
 }
 
+var createBoard = function () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'img/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+  }
+}
 
-
-
-flipCard(0)
-flipCard(2)
-
+createBoard()
